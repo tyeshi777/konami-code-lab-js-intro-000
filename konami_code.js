aeafdaefe5e80b955f95696a10f710b794eb24be
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-30 20:43:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,23 @@ const codes = [
 
 function init() {
   // your code here
+    let index = 0;
+    document.body.addEventListener('keydown', onKeyDownHandler = e => { 
+         for (let i = 0; i < codes.length; i++) {
+         const key = e.key;
+         if (key === codes[i]) {
+          index++;
+          if (index === codes.length) {
+            alert('Hurray!');
+            index = 0;
+          }
+        } else {
+          index = 0;
+        }
+        
+      }
+      
+    });
+ return index;
 }
+init();
